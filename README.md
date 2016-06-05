fa8be1040f938ec861ea1f789ed6335be28465ee
Robin Eggenkamp <robin@edubits.nl>
2016-06-05 12:10:42 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# Zway-MQTT
+# MQTT Z-Way HA module
+
+Publishes the status of devices to a MQTT topic and is able to set
+values based on subscribed topics.
+
+It is possible to publish the status for single devices, or for devices
+which are tagged. Per publication you can customize the topic and decide
+if the message should be retained or not.
+
+# Installation
+
+Make sure that the [BaseModule](https://github.com/maros/Zway-BaseModule) is installed prior to installing this 
+module.
+
+The preferred way of installing this module is via the "Zwave.me App Store"
+available in 2.2.0 and higher. For stable module releases no access token is 
+required. If you want to test the latest pre-releases use 'mqtt_beta' as 
+app store access token.
+
+For developers and users of older Z-Way versions installation via Git is 
+recommended.
+
+```shell
+cd /opt/z-way-server/automation/userModules
+git clone https://github.com/Edubits/Zway-MQTT.git MQTT --branch latest
+```
+
+To update or install a specific version
+```shell
+cd /opt/z-way-server/automation/userModules/MQTT
+git fetch --tags
+# For latest released version
+git checkout tags/latest
+# For a specific version
+git checkout tags/1.0
+# For development version
+git checkout -b master --track origin/master
+```
+
+# Usage
+
+Add an instance of the app through and fill in the details about your
+MQTT broker. After this configure for which devices you want to publish
+the status to MQTT. You can either configure this for a single device
+or for all devices tagged with a certain tag at once.
+
+## Defining the publication topic
+
+You can define the topic used for publication. It's constructed from the
+following parts:
+
+* Topic Prefix – The start of each topic
+* Topic – Per publication defined topic
+
+In each of these you can use `%deviceName` and `%roomName%` as variables.
+For example having `Foo/bar` as prefix and `%roomName%/%deviceName` as 
+topic can result in a message published on the topic 
+`Foo/bar/livingRoom/dimmer`. Both `%deviceName` and `%roomName%` will be
+camelcased.
+
+## Interacting through MQTT
+
+You can also publish to certain topics to interact with Z-Way. For this
+the two postfixes are used:
+
+* Status postfix (default: status)
+* Set postfix (default: set)
+
+Taking the same example as before, when you publish an empty message to 
+`Foo/bar/livingRoom/dimmer/status` the current value will be published
+again. When you publish a value (for example `on`, `off` or `87`) to 
+`Foo/bar/livingRoom/dimmer/set` the dimmer will be set to that level.
+
+# Acknowledgements
+
+I want to thank @goodfield for finding and fixing a fully JavaScript
+MQTTClient which I could use in this module as well. His module can be
+found at https://github.com/goodfield/zway-mqtt.
---
+++ index.js
@@ -0,0 +1,216 @@
+/*** MQTT Z-Way HA module ****************************************************
+
+Version: 1.0
+(c) Robin Eggenkamp, 2016
+-----------------------------------------------------------------------------
+Author: Robin Eggenkamp <robin@edubits.nl>
+Description:
+   Publishes the status of devices to a MQTT topic and is able
+   to set values based on subscribed topics
+
+   MQTTClient based on https://github.com/goodfield/zway-mqtt
+
+ *****************************************************************************/
+
+
+// ----------------------------------------------------------------------------
+// --- Class definition, inheritance and setup
+// ----------------------------------------------------------------------------
+
+function MQTT (id, controller) {
+    MQTT.super_.call(this, id, controller);
+}
+
+inherits(MQTT, BaseModule);
+
+_module = MQTT;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+
+MQTT.prototype.init = function (config) {
+    // Call superclass' init (this will process config argument and so on)
+    MQTT.super_.prototype.init.call(this, config);
+
+    var self = this;
+
+	self.initMQTTClient();
+
+	self.callback = _.bind(self.updateDevice, self);
+	self.controller.devices.on("modify:metrics:level", self.callback);
+};
+
+MQTT.prototype.stop = function () {
+	var self = this;
+
+	self.controller.devices.off("modify:metrics:level", self.callback);
+
+    MQTT.super_.prototype.stop.call(this);
+};
+
+// ----------------------------------------------------------------------------
+// --- Module methods
+// ----------------------------------------------------------------------------
+
+MQTT.prototype.initMQTTClient = function () {
+	var self = this;
+
+	executeFile(self.moduleBasePath() + "/lib/buffer.js");
+	executeFile(self.moduleBasePath() + "/lib/mqtt.js");
+	
+	self.client  = new MQTTClient(self.config.host, parseInt(self.config.port), {client_id: self.config.clientId});
+
+	self.client.connect(function () {
+		self.log("Connected to " + self.config.host);
+
+		self.client.subscribe(self.createTopic("/#"), {}, function (topic, payload) {
+			var topic = topic.toString();
+
+			if (!topic.endsWith(self.config.topicPostfixStatus) && !topic.endsWith(self.config.topicPostfixSet)) {
+				return;
+			}
+
+			self.controller.devices.each(function (device) {
+				self.processPublicationsForDevice(device, function (device, publication) {
+					var deviceTopic = self.createTopic(publication.topic, device);
+
+					if (topic == deviceTopic + "/" + self.config.topicPostfixStatus) {
+						self.updateDevice(device);
+					}
+
+					if (topic == deviceTopic + "/" + self.config.topicPostfixSet) {
+						var deviceType = device.get('deviceType');
+
+						if (deviceType.startsWith("sensor")) {
+							self.error("Can't perform action on sensor " + device.get("metrics:title"));
+							return;
+						}
+
+						if (deviceType === "switchMultilevel" && payload !== "on" && payload !== "off") {
+							self.log();
+							device.performCommand("exact", {level: payload + "%"});
+						} else {
+							device.performCommand(payload);
+						}
+					}
+				});
+			});
+		});
+	});
+
+	self.client.onError(function (error) {
+		self.error(error.toString());
+	});
+
+	self.client.onDisconnect(function () {
+		self.error("Disconnected, will retry to connect...");
+		self.connect();
+	});
+};
+
+MQTT.prototype.updateDevice = function (device) {
+	var self = this;
+
+	var value = device.get("metrics:level");
+
+	if (device.get("deviceType") == "switchBinary" || device.get("deviceType") == "sensorBinary") {
+		if (value == 0) {
+			value = "off";
+		} else if (value == 255) {
+			value = "on";
+		}
+	}
+
+	self.processPublicationsForDevice(device, function (device, publication) {
+		var topic = self.createTopic(publication.topic, device);
+
+		self.publish(topic, value, publication.retained);
+	});
+};
+
+MQTT.prototype.processPublicationsForDevice = function (device, callback) {
+	var self = this;
+
+	if (! _.isFunction(callback)) {
+		self.error('Invalid callback for processPublicationsForDevice');
+		return;
+	}
+
+	_.each(self.config.publications, function (publication) {
+		switch (publication.type) {
+			case "tag":
+				if (_.intersection(publication.tags, device.get("tags")).length > 0) {
+					callback(device, publication);
+				}
+				break;
+			case "single":
+				if (publication.deviceId == device.id) {
+					callback(device, publication);
+				}
+				break;
+		}
+	});
+};
+
+MQTT.prototype.publish = function (topic, value, retained) {
+	var self = this;
+
+	if (self.client && self.client.connected) {
+		var options = {};
+		options.retain = retained;
+
+		self.client.publish(topic, value.toString().trim(), options);
+	}
+};
+
+MQTT.prototype.createTopic = function (pattern, device) {
+	var self = this;
+
+	var topicParts = [].concat(self.config.topicPrefix.split("/"))
+		.concat(pattern.split("/"));
+
+	if (device != undefined) {
+		topicParts = topicParts.map(function (part) {
+			return part.replace("%roomName%", self.findRoom(device.get("location")).title.toCamelCase())
+					   .replace("%deviceName%", device.get("metrics:title").toCamelCase());
+
+			return part;
+		});
+	}
+
+	return topicParts.filter(function (part) {
+		return part !== undefined && part.length > 0;
+	}).join("/");
+};
+
+MQTT.prototype.findRoom = function (roomId) {
+	var self = this;
+
+	var locations = self.controller.locations;
+	if (locations) {
+		return locations.filter(function (location) {
+			return location.id == roomId;
+		})[0];
+	}
+	return undefined;
+};
+
+// ----------------------------------------------------------------------------
+// --- Utility methods
+// ----------------------------------------------------------------------------
+
+String.prototype.toCamelCase = function() {
+	return this
+		.replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
+		.replace(/\s/g, '')
+		.replace(/^(.)/, function($1) { return $1.toLowerCase(); });
+};
+
+String.prototype.startsWith = function (s) {
+	return this.length >= s.length && this.substr(0, s.length) == s;
+};
+
+String.prototype.endsWith = function (s) {
+	return this.length >= s.length && this.substr(this.length - s.length) == s;
+};
